14951b758c39b16ad1160091d79030edf604415c
CauanVSVV <cauan.valadares.svv@gmail.com>
2025-05-06 17:37:00 -0300

    Add API service with user and transaction endpoints using axios

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { IUsuario } from "../types";
+import { ITransacao, IUsuario } from "../types";
 
 const api = axios.create({
     baseURL: "http://localhost:5000",
@@ -12,5 +12,15 @@ export const obterUsuario = async (): Promise<IUsuario[]> => {
 
 export const criarUsuario = async (usuario: Omit<IUsuario, "id">): Promise<IUsuario> => {
     const {data} = await api.post<IUsuario>("/usuarios", usuario)
+    return data
+}
+
+export const obterTransacoes = async (): Promise<ITransacao[]> => {
+    const {data} = await api.get<ITransacao[]>("/transacoes")
+    return data
+}
+
+export const criarTransacao = async (transacao: Omit<ITransacao, "id">): Promise<ITransacao> => {
+    const {data} = await api.post<ITransacao>("/transacoes", transacao)
     return data
 }
